23f7157c3877f65af40eb99d4091f72aaa14d4dd
Thomas Anchor <30919196+TAnchor@users.noreply.github.com>
2018-01-22 15:52:49 -0500

    Add first API files

---
+++ AddUser.php
@@ -0,0 +1,48 @@
+<?php
+
+	// Assumes the input is a JSON file in the format of {"username":"", "password":""}
+	
+	$inData = getRequestInfo();
+	
+	$uName = $inData["username"];
+	$pwd = $inData["password"];
+	
+	// Server info for connection
+	$servername = "localhost";
+	$dbUName = "username";
+	$dbPwd = "password";
+	$dbName = "COP4331";
+	
+	// Connect to database
+	$conn = new mysqli($servername, $dbUName, $dbPwd, $dbName);
+	if ($conn->connect_error){
+		returnWithError($conn->connect_error);
+	}
+	else{
+		$sql = "insert into Users (Username,Password) VALUES ('" . $Uname . "','" . $pwd . "')";
+		if( $result = $conn->query($sql) != TRUE )
+		{
+			returnWithError( $conn->error );
+		}
+		$conn->close();
+	}
+	
+	returnWithError("");
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendAsJson( $retValue );
+	}
+?>
---
+++ Login.php
@@ -0,0 +1,60 @@
+<?php
+
+	// Assumes the input is a JSON file in the format of {"username":"", "password":""}
+	
+	$inData = getRequestInfo();
+	
+	// Server info for connection
+	$servername = "localhost";
+	$dbUName = "username";
+	$dbPwd = "password";
+	$dbName = "COP4331";
+	
+	$id = 0;
+	$username = $inData["login"];
+	
+	// Connect to database
+	$conn = new mysqli($servername, $dbUName, $dbPwd, $dbName);
+	if ($conn->connect_error){
+		returnWithError($conn->connect_error);
+	}
+	else{
+		$sql = "SELECT User ID FROM Users where Username = '" . $inData["login"] . "' AND Password = '" . $inData["password"] . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0){
+			$row = $result->fetch_assoc();
+			$id = $row["ID"];
+		}
+		else{
+			returnWithError( "No Records Found" );
+		}
+		$conn->close();
+	}
+	
+	returnWithInfo($username, $id);
+
+	// Parse JSON file input
+	function getRequestInfo(){
+		return json_decode(file_get_contents('php://input'), true);
+	}
+	
+	// Send the user's username and ID as JSON
+	function sendAsJSON($obj){
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	// Return in the case of an error
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","error":"' . $err . '"}';
+		sendAsJson( $retValue );
+	}
+	
+	// Return and send the username and id
+	function returnWithInfo( $username, $id )
+	{
+		$retValue = '{"id":' . $id . ',"username":"' . $username . '","error":""}';
+		sendAsJson( $retValue );
+	}
+?>
