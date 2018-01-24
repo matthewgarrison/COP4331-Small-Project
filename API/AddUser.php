ef636c5a3c7382afc58509c75d10b98dbc52219b
Thomas Anchor <30919196+TAnchor@users.noreply.github.com>
2018-01-24 15:06:19 -0500

    Add checking if a username is already in use
    Also add password sanitizing

--- API/AddUser.php
+++ API/AddUser.php
@@ -1,11 +1,10 @@
 <?php
-
 	// Assumes the input is a JSON file in the format of {"username":"", "password":""}
 	
 	$inData = getRequestInfo();
 	
 	$username = trimAndSanitize($inData["username"]);
-	$password = $inData["password"];
+	$password = trimAndSanitize($inData["password"]);
 	
 	// Server info for connection
 	$servername = "localhost";
@@ -22,11 +21,19 @@
 		returnWithError($conn->connect_error);
 	}
 	else{
-		$sql = "insert into USERS (USERNAME,PASSWORD) VALUES ('" . $username . "','" . $password . "')";
-		if( $result = $conn->query($sql) != TRUE )
-		{
+		$sql = "SELECT USER_ID FROM USERS where USERNAME = '" . $username . "'";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0){
 			$error_occurred = true;
-			returnWithError( $conn->error );
+			returnWithError("Username already in use");
+		}
+		else{
+			$sql = "insert into USERS (USERNAME,PASSWORD) VALUES ('" . $username . "','" . $password . "')";
+			if( $result = $conn->query($sql) != TRUE )
+			{
+				$error_occurred = true;
+				returnWithError( $conn->error );
+			}
 		}
 		$conn->close();
 	}
@@ -35,19 +42,19 @@
 		returnWithError("");
 	}
 	
-	// Removes whitespice at the front and back, and removes single quotes and semi-colons
+	// Removes whitespace at the front and back, and removes single quotes and semi-colons
 	function trimAndSanitize($str){
 		$str = trim($str);
 		$str = str_replace("'", "", $str );
 		$str = str_replace(";", "", $str);
 		return $str;
 	}
-
+	
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
 	}
-
+	
 	function sendAsJson( $obj )
 	{
 		header('Content-type: application/json');
