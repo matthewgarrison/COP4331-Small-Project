9436a65a010fdc5f0b075f2c8e210a8e17773ce8
Kobee Raveendran <32165720+kobeeraveendran@users.noreply.github.com>
2018-01-25 15:05:47 -0500

    added searchContacts and addContacts functions
    subject to change based on appearance (tried making a list of contact search results rather than a dropdown)

--- API/main.js
+++ API/main.js
@@ -1,13 +1,14 @@
 var baseURL = "http://www.cop4331-2.com/API"
 
 var userID = 0;
-var firstName = "";
-var lastName = "";
+//var firstName = "";
+//var lastName = "";
 
 function doLogin() {
+
 	userID = 0;
-	firstName = "";
-	lastName = "";
+	//firstName = "";
+	//lastName = "";
 
 	var userName = document.getElementById("form-username").value;
 	var password = document.getElementById("form-password").value;
@@ -15,16 +16,15 @@ function doLogin() {
 	document.getElementById("loginResult").innerHTML = "";
 
 	// replace with appropriate varaible names
-	var payload = '{"login" : "' + userName + '", "password" : "' + password'"}';
+	var payload = '{"username" : "' + userName + '", "password" : "' + password'"}';
 
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", baseURL + "/Login.php", false);
-	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
 
 	try {
-		xhr.send();
+		xhr.send(payload);
 
-		// XMLHttpRequest.response vs. XMLHttpRequest.responseText?
 		var data = JSON.parse(xhr.responseText);
 		userID = data.id;
 
@@ -35,19 +35,19 @@ function doLogin() {
 		}
 
 		// may change when displayname is implemented
-		firstName = data.firstName;
-		lastName = data.lastName;
+		//firstName = data.firstName;
+		//lastName = data.lastName;
 
 		// implement displayName later
-		document.getElementById('displayName').innerHTML = firstName + " " + lastName;
+		//document.getElementById('displayName').innerHTML = firstName + " " + lastName;
 
 		document.getElementById('form-username').value = "";
 		document.getElementById('form-password').value = "";
 
 		// implement when more features complete
-		// showElement('access', true);
-		// showElement('loggedIn', true);
-		
+		//showElement('access', true);
+		//showElement('loggedIn', true);
+
 		// this ID only applies to the button; ask to update to ID for the entire form
 		showElement('form-login', false);
 	}
@@ -59,6 +59,7 @@ function doLogin() {
 }
 
 function showElement(id, flag) {
+
 	if(flag) {
 		document.getElementById(id).style.visibility = 'visible';
 		document.getElementById(id).style.display = 'block';
@@ -69,7 +70,87 @@ function showElement(id, flag) {
 	}
 }
 
-// todo
+function doLogout() {
+
+   userID = 0;
+
+   showElement('loggedIn', false);
+   showElement('login', true);
+   showElement('access', false);
+}
+
+function addContact() {
+
+   var name = document.getElementById('contactName').value;
+   var phone = document.getElementById('contactPhone').value;
+   var email = document.getElementById('contactEmail').value;
+   var notes = document.getElementById('contactNotes').value;
+
+   var payload = '{"uID" : "' + userID + '", "name" : "' + name + '", "phoneNumber" : "' + phone + '", "email" : "' + email + '", "notes" : "' + notes'"}';
+
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", baseURL + "/CreateContact.php", true);
+   xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+   try {
+      xhr.onreadystatechange = function() {
+         // if "OK" status-number, and request finished w/ response ready --> indicate success
+         if (xhr.status === XMLHttpRequest.DONE && xhr.response === 4) {
+            document.getElementById('addContactResult').innerHTML = "Contact added successfully.";
+         }
+      };
+
+      xhr.send(payload);
+
+   }
+   catch(error) {
+      document.getElementById('addContactResult').innerHTML = error.message;
+   }
+}
+
+
 function searchContacts() {
+   var target = document.getElementById('targetName').value;
 	document.getElementById("searchContacts").innerHTML = "";
+
+   var contactList = document.getElementById('contactList');
+   contactList.innerHTML = "";
+
+   var payload = '{"search" : "' + target + '", "uID" : "' + userID'"}';
+
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", baseURL + "/SearchContact.php", true);
+   xhr.setRequestHeader("Content-type", "application/json; charset = UTF-8");
+
+   try {
+      xhr.onreadystatechange = function() {
+         if(xhr.status === XMLHttpRequest.DONE && xhr.response === 4) {
+            showElement('contactList', true);
+
+            document.getElementById('contactSearchResult').innerHTML = "Contacts retrieved.";
+
+            var data = JSON.parse(xhr.responseText);
+
+			var list = document.createElement('ul');
+
+            var i;
+            for(i = 0; i < data.results.length; i++) {
+			   var item = document.createElement('li');
+			   item.setAttribute('list-style-type', 'none');
+			   item.appendChild(document.createTextNode(data.results[i]));
+			   list.appendChild(item);
+            }
+         }
+      };
+
+      xhr.send(payload);
+   }
+   catch(error) {
+      document.getElementById('contactSearchResult').innerHTML = error.message;
+   }
+}
+
+// todo
+function deleteContacts() {
+
 }
