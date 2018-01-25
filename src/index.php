98b72007f5864dd34e13e585f79f1d6d571f6a77
Shane Rhoads <Kepter@users.noreply.github.com>
2018-01-25 13:01:02 -0500

    Fixed weird spacing issues

--- src/index.php
+++ src/index.php
@@ -32,35 +32,34 @@
 
 			<h1 class="page-title" id="username" style="padding-bottom: 40px">Logged in as ""</h1>
 
-      <label for="form-username">Search</label>
+     			<label for="form-username">Search</label>
 			<form class="form-inline">
 				<div class="form-group">
-				  <input type="text" class="form-control" id="search-username" value="" name="username" style="width:500px"/>
-          <button class="btn btn-defualt" id="seach-submit"  style="margin-left:30px">Submit</button>
+					<input type="text" class="form-control" id="search-username" value="" name="username" style="width:500px"/>
+					<button class="btn btn-defualt" id="seach-submit"  style="margin-left:30px">Submit</button>
 				</div>
 			</form>
 
-      <h2 style="padding-top:45px"> Results: </h2>
+      			<h2 style="padding-top:45px"> Results: </h2>
 
-      <table class="table" id="search-table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Notes</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>John Smith</td>
-            <td>(123) 456-7890</td>
-            <td>john@smith.com</td>
-            <td>This is an example contact</td>
-          </tr>
-        </tbody>
-      </table>
-
+			<table class="table" id="search-table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Phone Number</th>
+						<th>Email</th>
+						<th>Notes</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>John Smith</td>
+						<td>(123) 456-7890</td>
+						<td>john@smith.com</td>
+						<td>This is an example contact</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
