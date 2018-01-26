94f24240bec1ed627c4f3d2b56bb90bf30e18c49
Thomas Anchor <30919196+TAnchor@users.noreply.github.com>
2018-01-26 14:11:22 -0500

    Update SearchContact.php

--- API/SearchContact.php
+++ API/SearchContact.php
@@ -1,6 +1,6 @@
 <?php
 	// Expects input in the form of {"search":"", "uID":""}
-	// The results field in the output JSON is an array of strings formatted as "Contact ID: name, phone number, email, notes"
+	// The results field in the output JSON is an array of strings formatted as "Contact ID, name, phone number, email, notes"
 	
 	$inData = getRequestInfo();
 	
